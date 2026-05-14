baeee2611536e25350091b82656dee5cf72ce094
LucasPlacentino <23436953+LucasPlacentino@users.noreply.github.com>
2026-05-14 16:37:34 +0200

    fix name of app

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
     //Q_INIT_RESOURCE(application);
 
     QApplication app(argc, argv);
-    QCoreApplication::setOrganizationName("Lucas Placentino and Salman Houdaibi");
-    QCoreApplication::setApplicationName("802.11ay Raytracing Simulator");
+    QCoreApplication::setOrganizationName("Lucas Placentino");
+    QCoreApplication::setApplicationName("5G channel modeling raytracing");
     QCoreApplication::setApplicationVersion(QT_VERSION_STR);
 
     MainWindow mainWin;
